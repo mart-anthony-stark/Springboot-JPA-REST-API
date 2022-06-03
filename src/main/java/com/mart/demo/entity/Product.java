872978dfa29d2f6fd047e316516bf80fe10f2065
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-03 22:00:18 +0800

    Add: CRUD

---
+++ src/main/java/com/mart/demo/entity/Product.java
@@ -0,0 +1,50 @@
+package com.mart.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
+public class Product {
+	@Id
+	@GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+    
+    
+}
